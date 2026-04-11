376177e1c7fe31a3a89c3cd15d62dd460c19ebd2
appsgeek <support@karajkom.com>
2026-04-11 14:58:16 +0530

    Demo app feedback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
 
     // Hilt
     implementation(libs.hilt.android)
+    implementation(libs.androidx.ui)
     kapt(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
--- app/src/main/java/com/demo/newedgedemo/ui/theme/Type.kt
+++ app/src/main/java/com/demo/newedgedemo/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package com.demo.newedgedemo.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -15,4 +16,11 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+)
+
+val GrammarInputTextStyle = TextStyle(
+    fontFamily = FontFamily.SansSerif,
+    fontWeight = FontWeight.W400,
+    fontSize = 28.sp,
+    color = Color.Black
 )
